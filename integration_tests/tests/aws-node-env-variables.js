64443576d552d0b07739f08a909027d61f7f44d4
とろゝ <toro-ponz@users.noreply.github.com>
2019-07-14 17:15:31 +0900

    fix test

--- integration_tests/tests/aws-node-env-variables.js
+++ integration_tests/tests/aws-node-env-variables.js
@@ -12,23 +12,23 @@ const samTemplate = yaml.safeLoad(fs.readFileSync(__dirname + path.sep + ".." + 
 
 describe("Tests for " + __filename, () => {
     it("Created the Lambda function resource", () => {
-      expect(samTemplate.Resources.CreateUser).to.not.be.null;
+      expect(samTemplate.Resources.FunctionWithEnvironmentVariablesDevCreateUser).to.not.be.null;
     });
 
     it("Function runtime is node", () => {
-      expect(samTemplate.Resources.CreateUser.Properties.Runtime).to.be.equals("nodejs4.3");
+      expect(samTemplate.Resources.FunctionWithEnvironmentVariablesDevCreateUser.Properties.Runtime).to.be.equals("nodejs8.10");
     });
 
     it("Code uri is a zip", () => {
-      expect(samTemplate.Resources.CreateUser.Properties.CodeUri.endsWith("function-with-environment-variables.zip")).to.be.true;
+      expect(samTemplate.Resources.FunctionWithEnvironmentVariablesDevCreateUser.Properties.CodeUri.endsWith("function-with-environment-variables.zip")).to.be.true;
     });
 
     it("Environment variables are set", () => {
-      expect(Object.keys(samTemplate.Resources.CreateUser.Properties.Environment.Variables).length).to.be.equals(3);
+      expect(Object.keys(samTemplate.Resources.FunctionWithEnvironmentVariablesDevCreateUser.Properties.Environment.Variables).length).to.be.equals(3);
     });
 
     it("Password iterations value is set correctly", () => {
-      expect(samTemplate.Resources.CreateUser.Properties.Environment.Variables.PASSWORD_ITERATIONS).to.be.equals(4096);
+      expect(samTemplate.Resources.FunctionWithEnvironmentVariablesDevCreateUser.Properties.Environment.Variables.PASSWORD_ITERATIONS).to.be.equals(4096);
       
     });
 });
